ae731d77c68571a6a181839528b3136e43517267
Qingying Meng <qmeng222@gmail.com>
2022-07-09 23:12:50 -0700

    refactored the if conditions as helper functions

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+# helper function 1:
 def print_board(entries):
     line = "+---+---+---+"
     output = line
@@ -15,14 +16,28 @@ def print_board(entries):
     print(output)
     print()
 
-
+# helper function 2:
 def game_over(b, i):
     print_board(b)
     print('GAME OVER')
     print(b[i], "has won")
     exit()
 
+# group helper functions:
+def is_row_winner(b, i):
+    return board[i] == board[i+1] and board[i+1] == board[i+2]
 
+def is_col_winner(b, i):
+    return board[i] == board[i+3] and board[i+3] == board[i+6]
+
+def is_dia1_winner(b, i): # 1 5 9
+    return board[i] == board[i+4] and board[i+4] == board[i+8]
+
+def is_dia2_winner(b, i): #  3 5 7
+    return board[i] == board[i+2] and board[i+2] == board[i+4]
+
+
+# game:
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 current_player = "X"
 
@@ -32,21 +47,21 @@ for move_number in range(1, 10):
     space_number = int(response) - 1
     board[space_number] = current_player
 
-    if board[0] == board[1] and board[1] == board[2]:
+    if is_row_winner(board, 0):
         game_over(board, 0)
-    elif board[3] == board[4] and board[4] == board[5]:
+    elif is_row_winner(board, 3):
         game_over(board, 3)
-    elif board[6] == board[7] and board[7] == board[8]:
+    elif is_row_winner(board, 6):
         game_over(board, 6)
-    elif board[0] == board[3] and board[3] == board[6]:
+    elif is_col_winner(board, 0):
         game_over(board, 0)
-    elif board[1] == board[4] and board[4] == board[7]:
+    elif is_col_winner(board, 1):
         game_over(board, 1)
-    elif board[2] == board[5] and board[5] == board[8]:
+    elif is_col_winner(board, 2):
         game_over(board, 2)
-    elif board[0] == board[4] and board[4] == board[8]:
+    elif is_dia1_winner(board, 0):
         game_over(board, 0)
-    elif board[2] == board[4] and board[4] == board[6]:
+    elif is_dia2_winner(board, 2):
         game_over(board, 2)
 
     if current_player == "X":
